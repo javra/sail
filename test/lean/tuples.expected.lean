d347bedc955409cf6ab84b9eeb16f09db541a1be
Jakob von Raumer <jakob@von-raumer.de>
2025-01-16 11:16:43 +0100

    add parantheses in exptected test outputs

--- test/lean/tuples.expected.lean
+++ test/lean/tuples.expected.lean
@@ -2,8 +2,8 @@ import Out.Sail.Sail
 
 open Sail
 
-def tuple1 : (Int × Int × (BitVec 2 × Unit)) :=
-  (3, 5, ((0b10 : BitVec 2), ()))
+def tuple1 : (Int × Int × ((BitVec 2) × Unit)) :=
+  (3, 5, ((0b10 : (BitVec 2)), ()))
 
 def initialize_registers : Unit :=
   ()
--- test/lean/typquant.expected.lean
+++ test/lean/typquant.expected.lean
@@ -2,10 +2,10 @@ import Out.Sail.Sail
 
 open Sail
 
-def foo (n : Int) : BitVec 4 :=
-  (0xF : BitVec 4)
+def foo (n : Int) : (BitVec 4) :=
+  (0xF : (BitVec 4))
 
-def bar (x : BitVec k_n) : BitVec k_n :=
+def bar (x : (BitVec k_n)) : (BitVec k_n) :=
   x
 
 def initialize_registers : Unit :=
