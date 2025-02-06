c2d58d8a7149fa0a5161d0ed10d03ade153a38c5
Jakob von Raumer <jakob@von-raumer.de>
2025-02-06 12:40:38 +0100

    Lean: support for implicit arguments in function application (#955)
    This closes #948.

---
+++ test/lean/implicit.expected.lean
@@ -0,0 +1,16 @@
+import Out.Sail.Sail
+
+open Sail
+
+abbrev SailM := StateM Unit
+
+/-- Type quantifiers: k_n : Int, m : Int, m ≥ k_n -/
+def EXTZ {m : _} (v : (BitVec k_n)) : (BitVec m) :=
+  (Sail.BitVec.zeroExtend v m)
+
+def foo (x : (BitVec 8)) : (BitVec 16) :=
+  (EXTZ x)
+
+def initialize_registers (lit : Unit) : Unit :=
+  ()
+
